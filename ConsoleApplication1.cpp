ce0a176677ae3389be335ce870136cfd064923c4
anzor1qqqqq <anzor.shadzhe@gmail.com>
2024-10-30 23:13:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string line;
+
+    ifstream inputFile(argv[1]);
+    ofstream outputFile(argv[2]);
+
+    if (!outputFile || !inputFile) {
+        cout << "Указаны неверные файлы";
+        return 0;
+    };
+
+    while (getline(inputFile, line)) {
+        int exitCode = system((string("mult.exe ") + line).c_str());
+
+        outputFile << exitCode << '\n';
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+    system((string("notepad.exe ") + argv[2]).c_str());
+
+    return 0;
+}
---
+++ mult.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	int res = 1;
+
+	for (int i = 1; i < argc; ++i) {
+		res *= atoi(argv[i]);
+	}
+
+	return res;
+}
